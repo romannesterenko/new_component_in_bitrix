5bebeb4935137372d3a97b25f83182f7cbbe425e
romannesterenko <romannesterenko87@gmail.com>
2018-03-20 11:03:38 +0300

    Create .description.php

---
+++ class/.description.php
@@ -0,0 +1,5 @@
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die(); $arComponentDescription = array(
+  "NAME" => GetMessage(“Тестовый компонент на D7”),
+  "DESCRIPTION" => GetMessage(“Выводим произвольные данные”),
+  );
+?>
